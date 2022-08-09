9527e277054b5fec1f00f22090e39779efbf952c
ElinaNi2 <nikolaevaelina1@gmail.com>
2022-08-09 20:54:55 +0300

    Добавила проект Vue

---
+++ vue-auth_01_08/src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<script>
+
+    import Userpage from "./Userpage"
+
+    export default {
+        name: 'TODOITEM',
+        props: ['title'],
+        emits: ['remove'],
+        components:
+            { Userpage }
+    }
+</script>
+
+<template>
+    <!--<li>-->
+    <div class="group-15 border-0-4px-black">
+        {{ title }}
+        <!-- <keep-alive> -->
+        <Userpage v-bind:title="this.title" v-bind:addnew="this.addnew"></Userpage>
+        <!-- </keep-alive> -->
+        <button @click="$emit('remove')" v-if="!addnew">Remove</button>
+    </div>
+    <!--</li>-->
+</template>
+
+<style scoped>
+    .group-15 {
+        -webkit-backdrop-filter: blur(15px) brightness(100%);
+        backdrop-filter: blur(15px) brightness(100%);
+        background-color: var(--white-32);
+        border-radius: 13px;
+        min-height: 139px;
+        /*            left: 428px;
+                position: absolute;
+                top: 259px;
+    */
+        min-width: 197px;
+        padding: 10px;
+        margin: 10px;
+        display: inline-block
+    }
+</style>
